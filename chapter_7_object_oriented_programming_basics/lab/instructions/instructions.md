566ead07d253c66ac654f92c12f0f6b00777fba2
AlexKomanov <akomanov88@gmail.com>
2023-06-02 16:38:48 +0300

    Adding the Modeling Data with Classes in Python lab

---
+++ chapter_7_object_oriented_programming_basics/lab/instructions/instructions.md
@@ -0,0 +1,10 @@
+# Instructions
+
+Implement two classes:
+
+- `Todo` Class
+- `TodoList` Class
+
+### Methods
+
+Implement the missing methods in order to make the `Doctest` tests are passed. 
---
+++ chapter_7_object_oriented_programming_basics/lab/instructions/starting_code.py
@@ -0,0 +1,33 @@
+class Todo:
+    """
+    Todo represents a task with a name, description, point value, and completed.
+
+    A new Todo should have a `completed` field that defaults to `False`.
+    All other attributes must be provided.
+
+    >>> todo = Todo(name='Get Lunch', description='Need to eat.', points=0)
+    >>> print(todo)
+    Get Lunch (Incomplete - 0 points): Need to eat.
+    >>> todo.completed = True
+    >>> print(todo)
+    Get Lunch (Complete - 0 points): Need to eat.
+    >>> todo2 = Todo(name='Test', description='Another todo', points=1, completed=True)
+    """
+    pass
+
+class TodoList:
+    """
+    TodoList wraps a list of Todo objects and implements some functionality that
+    utilize the information from the entire collection.
+
+    >>> todo = Todo(name='Get Lunch', description='Need to eat', points=0, completed=True)
+    >>> todo2 = Todo(name='Submit Talk Proposal', description='Write and submit talk for PyCon', points=3)
+    >>> todo_list = TodoList([todo, todo2])
+    >>> todo_list.average_points()
+    1.5
+    >>> todo_list.completed()
+    [Get Lunch (Complete - 0 points): Need to eat]
+    >>> todo_list.incomplete()
+    [Submit Talk Proposal (Incomplete - 3 points): Write and submit talk for PyCon]
+    """
+    pass
